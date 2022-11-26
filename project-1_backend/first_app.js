d3afb601d94f23440c1e0fb0d71f71385fa3d9c8
Prathamesh <prathamesh17170@gmail.com>
2022-11-27 00:11:57 +0530

    more of web development

---
+++ project-1_backend/first_app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 80;
+
+// For using static files
+app.use('/static', express.static('static'));
+
+//set the templete engine as pug
+app.set('view engine', 'pug')
+
+//set views directory
+app.set('views', path.join(__dirname, 'views'));
+
+app.get("/", (req, res) => {
+    const con = "prathamesh is bad boy"
+    const param = { 'title': 'pug is best package', 'content': con }
+    res.status(200).render('demo.pug', param);
+});
+app.listen(port, () => {
+    console.log(`this application started successfully on port ${port}`)
+});
