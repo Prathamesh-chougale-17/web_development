dac59e5173d7d0b983a911871a65e9624df00197
Prathamesh <prathamesh17170@gmail.com>
2022-11-27 17:33:48 +0530

    completion of making a successful website

--- express1/app.js
+++ express1/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 80;
 
@@ -18,7 +19,20 @@ app.get("/", (req, res) => {
     res.status(200).render('demo.pug', param);
 });
 app.post('/',(req,res)=>{
-    console.log(req.body);//res.body is object with entered information
+    //console.log(req.body);//res.body is object with entered information
+    name = req.body.name;
+    email = req.body.email;
+    gender = req.body.gender;
+    address = req.body.address;
+    health = req.body.health;
+    console.log(gender);
+    console.log(name);
+    let output=`The name of person is ${name}
+The gender of ${name} is ${gender}
+The email of ${name} is ${email}
+The address of ${name} is ${address}
+The health of ${name} is ${health}`
+    fs.writeFileSync('output.txt',output);
     const param = { 'message': 'Your response has been recieved'}
     res.status(200).render('demo.pug',param)
 });
