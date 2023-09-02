ce0a3f47194325cfcc1c234c58fd67353dd6ff7d
Prathamesh-chougale-17 <prathamesh17170@gmail.com>
2023-09-02 23:56:29 +0530

    changes in frontend

--- project/portpolio/next_backend/schemas/Blog.js
+++ project/portpolio/next_backend/schemas/Blog.js
@@ -34,6 +34,30 @@ export default {
                     type: 'image'
                 }
             ]
+        },
+        {
+            title: "Blog Image",
+            name: "blogimage",
+            type: "image",
+            options: {
+                hotspot: true, // <-- Defaults to false
+            },
+            fields: [
+                {
+                    name: "caption",
+                    type: "string",
+                    title: "Caption",
+                    options: {
+                        isHighlighted: true, // <-- make this field easily accessible
+                    },
+                },
+                {
+                    // Editing this field will be hidden behind an "Edit"-button
+                    name: "attribution",
+                    type: "string",
+                    title: "Attribution",
+                },
+            ],
         }
     ]
 }
